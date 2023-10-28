b663bf58a7efeef5d02d24f95408892e7cb64000
Edgardo <edgardocruz62@yahoo.com>
2023-10-28 00:00:07 -0600

    Tamaño de inventario corregido

--- frontend/admin/src/pages/view/viewproducts.jsx
+++ frontend/admin/src/pages/view/viewproducts.jsx
@@ -21,6 +21,7 @@ const FoundProduct = ({ inputs, title }) => {
                 <div>
                     <Navbar />
                 </div>
+                <div className="new2">
                 <div className="h1">
                     <h1>Ver Producto</h1>
                 </div>
@@ -70,16 +71,18 @@ const FoundProduct = ({ inputs, title }) => {
                         </div>
                         <div className="right1">
                             <div className="contenido-derecha ">
-                                <label className="label1" htmlFor="model">Modelo:</label>
-                                <input className="input1" style={{ width: "290px" }} type="text" value={data && data.model ? data.model : ''} readOnly />
-                                <label className="label1" htmlFor="isv">ISV:</label>
-                                <input className="input1" style={{ width: "290px" }} type="number" value={data && data.tax ? data.tax : ''} readOnly />
+                                <label className="label3" htmlFor="model">Modelo:</label>
+                                <input className="input4" style={{ width: "300px" }} type="text" value={data && data.model ? data.model : ''} readOnly />
+                                <label className="label3" htmlFor="isv">ISV:</label>
+                                <input className="input4" style={{ width: "300px" }} type="number" value={data && data.tax ? data.tax : ''} readOnly />
+                                <label className="label3"></label>
                                 <label className="label2" htmlFor="descuento">Descuento:</label>
-                                <input className="input1" style={{ width: "50px" }} type="number" value={data && data.discount !== null ? data.discount : ''} readOnly />
+                                <input className="input4" style={{ width: "50px" }} type="number" value={data && data.discount !== null ? data.discount : ''} readOnly />
                                 <label className="label2" htmlFor="existencia">Existencia:</label>
-                                <input className="input1" style={{ width: "50px" }} type="number" value={data && data.existence ? data.existence : ''} readOnly />
-                                <label className="label1" htmlFor="marca">Marca:</label>
-                                <input className="input1" style={{ width: "290px" }} type="text" value={data && data.brand ? data.brand : ''} readOnly />
+                                <input className="input4" style={{ width: "50px" }} type="number" value={data && data.existence ? data.existence : ''} readOnly />
+                                <label className="label3" htmlFor="marca">Marca:</label>
+                                <input className="input4" style={{ width: "300px" }} type="text" value={data && data.brand ? data.brand : ''} readOnly />
+                                <label className="label3"></label>
                                 <Link to="/products">
                                     <button className="button1">Volver</button>
                                 </Link>
@@ -88,6 +91,7 @@ const FoundProduct = ({ inputs, title }) => {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
