4a41af636157e30bf606f1357175e7fdd59d958a
Edgardo <edgardocruz62@yahoo.com>
2023-11-30 19:37:05 -0600

    Darkmode se guarda en localstorage

--- frontend/admin/src/context/darkModeContext.js
+++ frontend/admin/src/context/darkModeContext.js
@@ -1,14 +1,19 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import DarkModeReducer from "./darkModeReducer";
 
 const INITIAL_STATE = {
-  darkMode: false,
+  darkMode: localStorage.getItem("darkMode") === "true" || false,
 };
 
 export const DarkModeContext = createContext(INITIAL_STATE);
 
 export const DarkModeContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+
+  // Guardar en el localStorage cuando cambia el estado
+  useEffect(() => {
+    localStorage.setItem("darkMode", state.darkMode);
+  }, [state.darkMode]);
 
   return (
     <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>
