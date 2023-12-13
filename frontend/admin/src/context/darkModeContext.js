1c8fdf17d45174c587eaeb0c878c08519ff8f503
Edgardo <edgardocruz62@yahoo.com>
2023-12-13 16:10:09 -0600

    personalizacion independiente

--- frontend/admin/src/context/darkModeContext.js
+++ frontend/admin/src/context/darkModeContext.js
@@ -1,22 +1,38 @@
+// darkModeContext.js
 import { createContext, useReducer, useEffect } from "react";
 import DarkModeReducer from "./darkModeReducer";
 
-const INITIAL_STATE = {
-  darkMode: localStorage.getItem("darkMode") === "true" || false,
+const getInitialDarkMode = (userId) => {
+  const userDarkMode = localStorage.getItem(`userDarkMode_${userId}`);
+  return userDarkMode === "true" || false;
 };
 
-export const DarkModeContext = createContext(INITIAL_STATE);
+export const DarkModeContext = createContext();
 
 export const DarkModeContextProvider = ({ children }) => {
+  const userId = sessionStorage.getItem('userID');
+  
+  const getInitialMode = () => {
+    const storedUserId = localStorage.getItem('currentUserId');
+    return userId === storedUserId ? getInitialDarkMode(userId) : false;
+  };
+
+  const INITIAL_STATE = {
+    darkMode: getInitialMode(),
+  };
+
   const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
 
-  // Guardar en el localStorage cuando cambia el estado
   useEffect(() => {
-    localStorage.setItem("darkMode", state.darkMode);
-  }, [state.darkMode]);
+    localStorage.setItem(`userDarkMode_${userId}`, state.darkMode);
+  }, [state.darkMode, userId]);
+
+  const updateInitialMode = (newMode) => {
+    dispatch({ type: 'SET_INITIAL_MODE', payload: newMode });
+  };
 
   return (
-    <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>
+    <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch, updateInitialMode }}>
       {children}
     </DarkModeContext.Provider>
   );
